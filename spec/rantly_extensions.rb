b603efac07f6a6283430f512c339be9696a383ab
Jessica Kerr <jessitron@gmail.com>
2014-08-09 21:19:51 -0500

    Randomly chunking method added to rantly

--- spec/rantly_extensions.rb
+++ spec/rantly_extensions.rb
@@ -4,16 +4,16 @@ class Rantly
 
   def chunk(array, qty)
     Enumerator.new do |yielder|
-      def bite(yielder, array, chunks)
-        if (chunks == 1) then
-          yielder.yield array
-        else
-          yield_this_many = range(0, array.size)
-          yielder.yield array.take(yield_this_many)
-          bite(yielder, array.drop(yield_this_many), chunks - 1)
-        end
+      chunks = qty #ow, mutation. But recursion stack-overflows
+      remaining = array
+      while(chunks > 1)
+        yield_this_many = range(0, array.size)
+        yielder.yield remaining.take(yield_this_many)
+
+        remaining = remaining.drop(yield_this_many)
+        chunks = chunks - 1
       end
-      bite(yielder, array, qty)
+      yielder.yield remaining
     end.to_a
   end
 end
--- spec/rantly_extensions_spec.rb
+++ spec/rantly_extensions_spec.rb
@@ -4,11 +4,14 @@ describe Rantly do
 
   context "chunking an array into random bits" do
 
+    # this is an example of a boxing-in test.
+    # It's random, so I can't check the output.
+    # But I can put it in a box, over and over.
     it("always returns the same items in the same order") do
       property_of {
         Generators.of_two(
           Generators.any_number_of(Generators.things),
-          Generator.new(->() { Generators.rantly.range(1)})
+          Generator.new(->() { Generators.rantly.range(1, 15000)})
         ).sample
       }.check { |(input,qty)|
         result = Rantly.new.chunk(input, qty)
