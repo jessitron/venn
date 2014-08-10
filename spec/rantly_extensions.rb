36ba330984ff1e03c749fee2004038291bf8d71b
Jessica Kerr <jessitron@gmail.com>
2014-08-09 21:05:06 -0500

    working on recursive

--- spec/rantly_extensions.rb
+++ spec/rantly_extensions.rb
@@ -3,6 +3,16 @@ require 'rantly'
 class Rantly
 
   def chunk(array)
-    array
+    Enumerator.new do |yielder|
+      def bite(yielder, (head, *tail))
+        yielder.yield [head]
+        if (not tail.empty?) then
+          bite(yielder, tail)
+        end
+      end
+      if (array.empty?) then []
+      else      bite(yielder, array)
+      end
+    end.to_a
   end
 end
